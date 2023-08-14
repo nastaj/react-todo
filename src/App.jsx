da26fe9ca6598f94b3cd970a61758eabbb3f069d
nastaj <G00424689@atu.ie>
2023-08-14 15:51:17 +0200

    folder structure setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import "./assets/scss/app.scss";
+import ToDo from "./components/ToDo";
+
 function App() {
-  return <div>Hello React!</div>;
+  return (
+    <>
+      <div className="background-top"></div>
+      <div className="background-bottom"></div>
+      <ToDo />
+    </>
+  );
 }
 
 export default App;
