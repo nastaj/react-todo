7a0c925ca21f96dc8d4c7ed7873de21806d16ffb
nastaj <G00424689@atu.ie>
2023-08-16 19:05:41 +0200

    media queries

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -5,12 +5,15 @@ export default function Summary({ todos, onClearComplete, view, onView }) {
   const itemsLeft = todos.filter((todo) => !todo.completed).length;
 
   return (
-    <footer className="summary">
-      {itemsLeft === 0 && <span>All tasks complete!</span>}
-      {itemsLeft === 1 && <span>{itemsLeft} item left</span>}
-      {itemsLeft > 1 && <span>{itemsLeft} items left</span>}
+    <>
+      <footer className="summary">
+        {itemsLeft === 0 && <span>All tasks complete!</span>}
+        {itemsLeft === 1 && <span>{itemsLeft} item left</span>}
+        {itemsLeft > 1 && <span>{itemsLeft} items left</span>}
+
+        <button onClick={onClearComplete}>Clear completed</button>
+      </footer>
       <Filters view={view} onView={onView} />
-      <button onClick={onClearComplete}>Clear completed</button>
-    </footer>
+    </>
   );
 }
